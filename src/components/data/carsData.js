2c765a1482e2f64cfba4b5394897508a76f357ca
Muhammad Siddiq Fathurahman <129701382+siddiqfathurahman@users.noreply.github.com>
2025-06-01 21:20:35 +0700

    update data cars

--- src/components/data/carsData.js
+++ src/components/data/carsData.js
@@ -206,9 +206,6 @@ export const cars = [
       "Leather Seats": true,
       GPS: true,
     },
-    description:
-      "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way",
-    review: "Excellent performance and luxury feel.",
   },
   {
     id: 12,
